e866502cd021cc43a2b389a5ab63691831225ab8
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-16 18:12:55 -0500

    Got the movies to render from the data, still trying to figure out how to make the cards appear inline and 3 per row.

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ document.addEventListener('DOMContentLoaded',function(){
 // console.log(movieData)
 function renderMovies(movieArray) {
   let movieHTML = movieArray.map(function(currentMovie){
-    return `<div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${currentMovie.Title}</h5>
-        <p class="card-text">${currentMovie.Year}</p>
-        <a href="#" class="btn btn-primary">Add</a>
-      </div>
-    </div>	
-  </div>`
+    return `
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 mt-3">
+            <div class="card-deck">
+              <div class="card" style="width: 50px;">
+                <img class="card-img-top" src="${currentMovie.Poster}" alt="Movie Poster">
+                <div class="card-body">
+                  <h5 class="card-title">${currentMovie.Title}</h5>
+                  <p class="card-text">${currentMovie.Year}</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                </div>
+              </div>	
+            </div>
+          </div>
+        </div>
+      </div>`
   })
   return movieHTML.join("")
 }
