3969d01151e07459fdf4848a125ab7dfba22c9ea
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-16 17:25:40 -0500

    Finished Step 2. The issue seemed to be that innerHTML at the end wasn't looking at an ID but a class name. By switching it to ID the movies were able to be rendered.

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ document.addEventListener('DOMContentLoaded',function(){
 
 // console.log(movieData)
 function renderMovies(movieArray) {
-  let movieHTML = movieArray.map(renderMovie)
+  let movieHTML = movieArray.map(function(currentMovie){
+    return `<div class="card-deck">
+    <div class="card">
+      <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${currentMovie.Title}</h5>
+        <p class="card-text">${currentMovie.Year}</p>
+        <a href="#" class="btn btn-primary">Add</a>
+      </div>
+    </div>	
+  </div>`
+  })
   return movieHTML.join("")
 }
-
-function renderMovie(currentMovie) {
-  return `<div class="card-deck">
-            <div class="card">
-              <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">${currentMovie.Title}</h5>
-                <p class="card-text">${currentMovie.Year}</p>
-                <a href="#" class="btn btn-primary">Add</a>
-              </div>
-            </div>	
-          </div>`
-}
-
-
-
-$('.movie').html = renderMovies(movieData)
+var movieContainer = document.getElementById('movie')
+movieContainer.innerHTML = renderMovies(movieData)
 })
