49c454c476558f232e6c1d1e1bc48d84067e0b7c
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-17 12:14:37 -0500

    Got the movies to line up correctly. The error was that you had too many divs and was not giving the correct flex display option on the parent.

--- index.js
+++ index.js
@@ -4,25 +4,17 @@ document.addEventListener('DOMContentLoaded',function(){
 function renderMovies(movieArray) {
   let movieHTML = movieArray.map(function(currentMovie){
     return `
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12 mt-3">
-            <div class="card-deck">
-              <div class="card" style="width: 50px;">
+              <div class="card cardStyle">
                 <img class="card-img-top" src="${currentMovie.Poster}" alt="Movie Poster">
                 <div class="card-body">
                   <h5 class="card-title">${currentMovie.Title}</h5>
                   <p class="card-text">${currentMovie.Year}</p>
                   <a href="#" class="btn btn-primary">Add</a>
                 </div>
-              </div>	
-            </div>
-          </div>
-        </div>
-      </div>`
+              </div>`
   })
   return movieHTML.join("")
 }
-var movieContainer = document.getElementById('movie')
+var movieContainer = document.getElementById('movies-container')
 movieContainer.innerHTML = renderMovies(movieData)
 })
