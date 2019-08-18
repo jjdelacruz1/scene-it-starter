0fb5ce1f01ca1c329489b6c6f09048a33cdcd351
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-18 15:27:11 -0500

    Got up to Step 2 of Part 2 with the help of Chris. Using the instructions did not help with setting up the click listener so he just refactored everything like crazy and this is what it looks like now. On to the next part.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded',function(){
 
 // console.log(movieData)
 function renderMovies(movieArray) {
@@ -9,12 +8,45 @@ function renderMovies(movieArray) {
                 <div class="card-body">
                   <h5 class="card-title">${currentMovie.Title}</h5>
                   <p class="card-text">${currentMovie.Year}</p>
-                  <a href="#" class="btn btn-primary">Add</a>
+                  <button type="button" id="addBtn" data-movieid="${currentMovie.imdbID}" class="btn btn-primary btn-block add-movie-btn">Add</a>
                 </div>
               </div>`
   })
   return movieHTML.join("")
 }
-var movieContainer = document.getElementById('movies-container')
-movieContainer.innerHTML = renderMovies(movieData)
-})
+
+// function saveToWatchlist(imdbID){
+//   console.log("save to watchlist clicked!", imdbID)
+// }
+
+function clickMoviesContainer (evt) {
+  // only do something when they click on a .add-movie-btn
+  const targetEl = evt.target
+  if (!targetEl.classList.contains('add-movie-btn')) return
+
+  // make sure the btn has a data-movieid attribute
+  const movieId = targetEl.dataset.movieid
+  if (typeof movieId !== 'string') return
+
+  console.log('you clicked on movie id: ', movieId)
+
+}
+
+function addEvents() {
+  byId('search-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    var movieContainer = document.getElementById('movies-container')
+    movieContainer.innerHTML = renderMovies(movieData)
+  })
+  byId('movies-container').addEventListener('click', clickMoviesContainer)
+}
+
+function byId (id) {
+  return document.getElementById(id)
+}
+
+function init () {
+  addEvents()
+}
+
+document.addEventListener('DOMContentLoaded', init)
